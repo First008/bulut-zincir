a240b978df18fc70a3d02884c8ea26a8c8f279a1
ahmet.birinci <ayb84870@gmail.com>
2021-09-22 11:16:27 +0300

    Account oluşturma ve genesise ekleme başarılı

--- kubernetes/Dev-Geth/genesis-files/priv-key-resolver.js
+++
@@ -1,8 +0,0 @@
-var keythereum = require("keythereum");
-var datadir = "/home/orta/Documents/bulut-zincir/kubernetes/imti-geth/genesis-files";
-var address= "0x8E24c067e098c2DaDF28aec75A551257d2C9316f";
-const password = "qwer";
-
-var keyObject = keythereum.importFromFile(address, datadir);
-var privateKey = keythereum.recover(password, keyObject);
-console.log(privateKey.toString('hex'));
--- kubernetes/Dev-Geth/genesis-files/pub-key-from-priv.js
+++
@@ -1,5 +0,0 @@
-const privateKeyToPublicKey = require('ethereum-private-key-to-public-key')
- 
-console.log(privateKeyToPublicKey('383c13ad301f0879ff5ff26355377c386f23cbfa3832273593350a5eb1129bb4').toString('hex'))
-console.log(privateKeyToPublicKey('6c429aea17cdae0ea1a9de40455244ab8e9e79b887cf65897f1c2d86604f634a').toString('hex'))
-console.log(privateKeyToPublicKey('a9e81443dc8e8054774d1e7321f43c1fe1699788e1ee7289f650cb8f7d4d563a').toString('hex'))
